9104f83f305d928e1237fefcfc3d59f365020cc6
adithyakam <50355901+adithyakam@users.noreply.github.com>
2019-07-31 23:57:19 +0530

    Search bar and json

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React,{Component} from 'react';
+import { robots } from './robots';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import './App.css';
+
+
+class  App extends Component{
+
+constructor(){
+    super()
+    this.state={
+        robots:[],
+        searchfield:''
+    }
+}
+
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots:users}));
+}
+
+onSearchChange=(event)=>{
+    this.setState({searchfield:event.target.value})
+    console.log(event.target.value);
+
+}
+
+    render(){
+        const filterRobots=this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    
+        })
+        if(this.state.robots.length ===0){
+            return <h1 className='tc'>Loding</h1>
+        }
+        else{
+        return(
+         <div className='tc'>
+            <h1 className='f2'>RobotFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <CardList robots={filterRobots}/>
+        </div>
+         );
+        }
+    }
+}
+export default App;
--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card=({name,email,id})=>{
 
 return(
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5  '>
-        <img alt=''src={`https://robohash.org/${id}?200x200`}/>>
+        <img alt=''src={`https://robohash.org/${id}?200x200`} />>
     <div>
     <h2>{name}</h2>
     <p>{email}</p>
