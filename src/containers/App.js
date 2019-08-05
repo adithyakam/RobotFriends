1bca44377773710a32e3b39845766983aff80692
adithyakam <50355901+adithyakam@users.noreply.github.com>
2019-08-05 23:24:47 +0530

    react with buid and optimised code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React,{Component} from 'react';
-import { robots } from './robots';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import './App.css';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
+import SearchBox from '../components/SearchBox';
+import '../App.css';
+
 
 
 class  App extends Component{
@@ -28,22 +29,22 @@ onSearchChange=(event)=>{
 }
 
     render(){
-        const filterRobots=this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    
+        const { robots ,searchfield } = this.state;
+        const filterRobots=robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if(this.state.robots.length ===0){
-            return <h1 className='tc'>Loding</h1>
-        }
-        else{
-        return(
+        return  !robots.length ?
+             <h1 className='tc'>Loding</h1>:
+       (
          <div className='tc'>
             <h1 className='f2'>RobotFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots={filterRobots}/>
+             <SearchBox searchChange={this.onSearchChange} />
+            <Scroll>
+                 <CardList robots={filterRobots}/>
+            </Scroll>
         </div>
          );
-        }
+        
     }
 }
 export default App;
